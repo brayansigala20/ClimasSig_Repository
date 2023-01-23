07398bc7cd3e67b3291d006ba0de92d22d0c4b09
Brayan Sigala <brayansigala21@gmail.com>
2023-01-23 11:45:51 -0700

    añadiendo nueva rama

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {  
+  apiKey: "AIzaSyC7Q16PCCjtIWC83CDf1V5_Fvg3v1gH9u0",
+  authDomain: "climascrud.firebaseapp.com",
+  projectId: "climascrud",
+  storageBucket: "climascrud.appspot.com",
+  messagingSenderId: "859031453553",
+  appId: "1:859031453553:web:e2fb0c74bd4e1dba4740d0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
